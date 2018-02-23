6c34117cfab8b75413455899a3a34a38f5b7f17e
Christoper <celoranta@gmail.com>
2018-02-23 11:51:53 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Chris Eloranta on 2018-02-23.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TipCalculator
+//
+//  Created by Chris Eloranta on 2018-02-23.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
